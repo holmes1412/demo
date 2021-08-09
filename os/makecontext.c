3f0749f3dc5fd39e898f85de17c834735399780c
holmes1412 <liyingxin1412@gmail.com>
2021-08-09 23:41:04 +0800

    add makecontext

---
+++ os/makecontext.c
@@ -0,0 +1,41 @@
+#include <ucontext.h> 
+#include <stdio.h>
+#include <errno.h>
+
+void test_func(int num)
+{
+    printf("test func %d\n", num);
+}
+
+int main()
+{
+    char stack[10 * 1024]; // to small will segment fault
+
+    ucontext_t cur_ctx;
+    getcontext(&cur_ctx);
+
+	// 0. should init new_ctx before makecontext()
+    ucontext_t new_ctx = cur_ctx;
+    new_ctx.uc_stack.ss_sp = stack;
+    new_ctx.uc_stack.ss_size = sizeof(stack);
+    new_ctx.uc_stack.ss_flags = 0;
+
+	// 1. will go back to cur_ctx after test_func() return
+    new_ctx.uc_link = &cur_ctx;
+
+	cur_ctx.uc_link = &new_ctx; // will not cause dead looping. why?
+
+	// 2. make test_func() as new_ctx`s entry
+	// and make new_ctx as the test_func()`s context,
+	// so it wil return to it`s uc_link
+    makecontext(&new_ctx, (void (*)(void))test_func, 1, 1412); 
+
+   	printf("check errno=%d\n", errno);
+	// 3. swap current to new_ctx, which will call test_func()
+    swapcontext(&cur_ctx, &new_ctx);
+
+    printf("main\n");
+
+    return 0;
+}
+
--- os/setcontext.c
+++ os/setcontext.c
@@ -4,8 +4,11 @@
 int main()
 {
 	ucontext_t ctx;
+	int i = 0;
 	getcontext(&ctx);
-	printf("this will go on forever\n");
-	setcontext(&ctx);
+	printf("this will go on. i = %d\n", i);
+	if (++i < 2)
+		setcontext(&ctx);
 	return 0;
 }
+
